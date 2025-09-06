7bba5d8ff66b086fa011f11ff57441de5778da0b
TUSHAR-hase <thakortushar81315@gmail.com>
2025-09-06 20:12:54 +0530

    Your commit message

--- Rail_Madad_Backend/schemas/userschemas.js
+++ Rail_Madad_Backend/schemas/userschemas.js
@@ -41,6 +41,6 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const User = mongoose.models.users||mongoose.model("users", userSchema);
+const User = mongoose.models.Users||mongoose.model("Users", userSchema);
 
 export default User;
