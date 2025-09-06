6c8d08555e67df7dec76d310155075587f92a452
TUSHAR-hase <thakortushar81315@gmail.com>
2025-09-06 21:35:42 +0530

    Your commit message

--- Rail_Madad_Backend/index.js
+++ Rail_Madad_Backend/index.js
@@ -34,7 +34,7 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.post("/api/complaints", authMiddleware, submitComplaint);
+// app.post("/api/complaints", authMiddleware, submitComplaint);
 app.use('/api/railmadad', userauth);
 app.use('/api/send/complainto', complain);
 app.use('/api/test', testapi);
--- Rail_Madad_Backend/routes/complaintroute.js
+++ Rail_Madad_Backend/routes/complaintroute.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { submitComplaint,statusupdate,getAllcomplaint} from "../api/complaint.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // POST /api/complaints
-router.post("/complaint", submitComplaint);
+router.post("/complaint",authMiddleware ,submitComplaint);
 router.get("/getcomplaint",getAllcomplaint);
 
 router.put("/complaint/:complaintId/resolved", statusupdate);
